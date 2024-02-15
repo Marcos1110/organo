7e283a4868eb2e475f57c593bcc9af70d461df09
Marcos1110 <marcosreis1110@gmail.com>
2024-02-15 20:01:45 -0300

    Dia 01

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import gif from './mission-complete.gif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={gif} className="App-logo" alt="logo" />
+        <p>
+          Hello World!
+        </p>
+        <p>
+          Esse é meu primeiro código em React!
+        </p>
+        <p>
+          #100DaysOfCode! - Day#001
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
